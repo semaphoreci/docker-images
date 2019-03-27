63fe12f52d606c4b5daf9884c0a3279caab899f0
Lukasz Rohde <lukasz.rohde@gmail.com>
2019-03-27 16:29:22 +0100

    pipeline test

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+for dir in */; do
+  repo=`basename ${dir}`
+  for file in $repo/*; do
+    dockerfile=`basename $file`
+    version=$(echo $dockerfile | awk -F"$repo-" '{print $2}')
+    docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir
+    docker push semaphoreci/$repo:${version//-/.}
+  done
+done
