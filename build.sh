885eac2b6914995cd3427b3182a0f05030139716
bogyo210 <nbors@renderedrext.com>
2019-03-29 15:07:35 +0200

    ,->.

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ for dir in */; do
     docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir || ((i++))
     case $repo in
       "ruby")
-          sed "s|_ruby_version_|${version//-/,}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
+          sed "s|_ruby_version_|${version//-/.}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
     esac
     docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
     if ! grep -q 'Failed: 0' /tmp/tmp/docker_output.log; then
