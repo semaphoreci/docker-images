e1926226cdb74b1f7be1e69760219dd71ae5756c
bogyo210 <nbors@renderedrext.com>
2019-03-29 09:18:51 +0200

    remove unnecessary dir

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ for dir in */; do
     dockerfile=`basename $file`
     version=$(echo $dockerfile | awk -F"$repo-" '{print $2}')
     docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir
-    docker ps | grep $repo:${version//-/.}
     time dgoss run semaphoreci/$repo:${version//-/.}
   done
 done
