c2060c641da097acea6674454389864814d3b4a2
bogyo210 <nbors@renderedrext.com>
2019-03-29 11:54:59 +0200

    use goss

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 set -x
-
+mkdir /tmp/tmp
 for dir in */; do
   repo=`basename ${dir}`
   for file in $repo/*; do
     dockerfile=`basename $file`
     version=$(echo $dockerfile | awk -F"$repo-" '{print $2}')
-    i=0
     docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir || ((i++))
-    time dgoss run -e DEBUG=true semaphoreci/$repo:${version//-/.} || ((i++))
-    if [ $i > 0 ];then 
-      #cat /goss/docker_output.log
+    echo "" > /tmp/tmp/docker_output.log 
+    cp $(which goss) /tmp/tmp/
+    i=0
+    cp goss.yaml /tmp/tmp/
+    docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate'> /tmp/tmp/docker_output.log 2> /tmp/tmp/docker_output.log || ((i++))
+    if [ $i > 0 ]; then 
+      echo "Error:"
+      cat /tmp/tmp/docker_output.log
       exit $i
     fi
-    ls -lah
   done
 done
