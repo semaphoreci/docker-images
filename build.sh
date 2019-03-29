3925369f7034bb37be758ffecf2bdfa6d6d41770
bogyo210 <nbors@renderedrext.com>
2019-03-29 13:26:20 +0200

    fail on purpose

--- build.sh
+++ build.sh
@@ -2,18 +2,23 @@
 
 set -x
 mkdir /tmp/tmp
+echo "" > /tmp/tmp/docker_output.log 
+cp $(which goss) /tmp/tmp/
+
 for dir in */; do
   repo=`basename ${dir}`
   for file in $repo/*; do
     dockerfile=`basename $file`
     version=$(echo $dockerfile | awk -F"$repo-" '{print $2}')
     docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir || ((i++))
-    echo "" > /tmp/tmp/docker_output.log 
-    cp $(which goss) /tmp/tmp/
     case $repo in
       "ruby")
-          sed "s|_ruby_version_|${version//-/.}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
+          sed "s|_ruby_version_|${version//-/,}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
     esac
-    docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' 
+    docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
+    if [[ grep 'Failed: 0' /tmp/tmp/docker_output.log ]]; then
+      cat /tmp/tmp/docker_output.log
+      exit 1
+    fi
   done
 done
