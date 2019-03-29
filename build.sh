f7947993041168f3b90dbe36ecba909fcd3298ad
semaphore-robot <semaphore.robot@gmail.com>
2019-03-29 11:02:13 +0100

    Update pipeline

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 
 set -x
 
-for dir in */; do
-  repo=`basename ${dir}`
-  for file in $repo/*; do
-    dockerfile=`basename $file`
-    version=$(echo $dockerfile | awk -F"$repo-" '{print $2}')
-    docker build -t semaphoreci/$repo:${version//-/.} -f $file $dir
-    docker push semaphoreci/$repo:${version//-/.}
-  done
+BUILD_DIR=$1
+
+for file in ${BUILD_DIR///}/*; do
+  dockerfile=`basename $file`
+  version=$(echo $dockerfile | awk -F"${BUILD_DIR///}-" '{print $2}')
+  docker build -t semaphoreci/${BUILD_DIR///}:${version//-/.} -f $file ${BUILD_DIR///}
+  docker push semaphoreci/${BUILD_DIR///}:${version//-/.}
 done
