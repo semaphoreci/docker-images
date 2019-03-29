c4d3e3eae62b2a2aabc3f4937d143f2ebca177d9
bogyo210 <nbors@renderedrext.com>
2019-03-29 12:08:41 +0200

    error handling correction

--- build.sh
+++ build.sh
@@ -12,11 +12,12 @@ for dir in */; do
     cp $(which goss) /tmp/tmp/
     i=0
     cp goss.yaml /tmp/tmp/
-    time docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log || ((i++))
-    if [ $i > 0 ]; then 
+    docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
+    dock=$?
+    if [ $dock > 0 ]; then 
       echo "Error:"
       cat /tmp/tmp/docker_output.log
-      exit $i
+      exit $dock
     fi
   done
 done
