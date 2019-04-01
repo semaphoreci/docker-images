770074bebaef547035d7e0a190feb93dcda59b99
bogyo210 <nbors@renderedrext.com>
2019-04-01 09:10:06 +0300

    remove debug

--- build.sh
+++ build.sh
@@ -16,13 +16,11 @@ for dir in */; do
           sed "s|_ruby_version_|${version//-/.}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
       "android")
           cat goss_android.yaml > /tmp/tmp/goss.yaml ;;
-
     esac
     docker run -v /tmp/tmp:/goss semaphoreci/$repo:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
     cat /tmp/tmp/docker_output.log
     if ! grep -q 'Failed: 0' /tmp/tmp/docker_output.log; then
       exit 1
     fi
-    exit 0
   done
 done
