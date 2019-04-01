944814c371154d4bb83011f9b8e11c0ad78099dd
bogyo210 <nbors@renderedrext.com>
2019-04-01 11:50:03 +0300

    goss dir for tests

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@ for file in ${BUILD_DIR///}/*; do
   docker build -t semaphoreci/${BUILD_DIR///}:${version//-/.} -f $file ${BUILD_DIR///}
   case $BUILD_DIR in
     "ruby")
-      sed "s|_ruby_version_|${version//-/.}|g" goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
+      sed "s|_ruby_version_|${version//-/.}|g" goss/goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
     "android")
-      cat goss_android.yaml > /tmp/tmp/goss.yaml ;;
+      cat goss/goss_android.yaml > /tmp/tmp/goss.yaml ;;
     esac
     docker run -v /tmp/tmp:/goss semaphoreci/${BUILD_DIR///}:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
     cat /tmp/tmp/docker_output.log
