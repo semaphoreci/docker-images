c90448bfbce78bca73fc72ae4ab5fb2bb76b6f39
bogyo210 <nbors@renderedrext.com>
2019-04-25 17:35:18 +0300

    verify ok

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ verify()
       sed "s|_ruby_version_|${version//-/.}|g" goss/goss_ruby.yaml > /tmp/tmp/goss.yaml ;;
     "android")
       cat goss/goss_android.yaml > /tmp/tmp/goss.yaml ;;
-    esac
-    docker run -v /tmp/tmp:/goss semaphoreci/${BUILD_DIR///}:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
+  esac
+  docker run -v /tmp/tmp:/goss semaphoreci/${BUILD_DIR///}:${version//-/.} sh -c 'cd /goss; ./goss validate' >/tmp/tmp/docker_output.log 2>/tmp/tmp/docker_output.log
   if ! grep -q 'Failed: 0' /tmp/tmp/docker_output.log; then
     echo 1
   else
