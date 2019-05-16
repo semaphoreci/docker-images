e5de694c9304166cd95912ebcaebfca86fdd8894
Lukasz Rohde <lukasz.rohde@gmail.com>
2019-05-16 23:10:18 +0200

    push to dockerhub

--- build.rb
+++ build.rb
@@ -4,7 +4,6 @@ require 'net/http'
 require 'optparse'
 require 'ostruct'
 require 'logger'
-require 'pathname'
 require 'open3'
 
 
@@ -32,9 +31,7 @@ class SemaphoreRegistry
 
   def self.run(cmd)
     Open3.popen2(cmd) do |stdin, stdout_stderr, wait_thr|
-      Thread.new do
-        stdout_stderr.each {|l| @logger.info l }
-      end
+      stdout_stderr.each {|l| @logger.info l }
       exit_status = wait_thr.value
       unless exit_status.success?
         abort "FAILED !!! #{cmd}"
@@ -42,31 +39,33 @@ class SemaphoreRegistry
     end
   end
 
-
-  def self.build(dir)
+  def self.build(dir,rebuild)
     files = Dir.entries(dir).select {|f| !File.directory? f }
     files.each do |f|
-      puts f
       # e.g Dockerfile-golang-1.9
       parts = f.split("-")
       repo = parts[1]
       # Get tag from filename - 1.9-node
       version = parts[2]
       tag = parts[2...parts.length].map { |k| k }.join("-")
-      if !self.search(repo,tag)
-        @logger.info("Building #{repo} #{tag}")
-        self.run("docker build -t semaphoreci/#{repo}:#{tag} -f #{dir}/#{f} #{dir}")
-        @logger.info("Running Tests")
-        self.run("GOSS_FILES_PATH=tests/goss GOSS_VARS=vars.yaml GOSS_FILES_STRATEGY=cp dgoss run -e PACKAGE=\"#{repo}\" -e VERSION=\"#{version}\" semaphoreci/#{repo}:#{tag} /bin/sleep 3600")
-      end
+      next if self.search(repo,tag) && !rebuild
+      @logger.info("Rebuilding all Images") if rebuild
+      @logger.info("Building #{repo} #{tag}")
+      self.run("docker build -t semaphoreci/#{repo}:#{tag} -f #{dir}/#{f} #{dir}")
+      @logger.info("Running Tests")
+      self.run("GOSS_FILES_PATH=tests/goss GOSS_VARS=vars.yaml GOSS_FILES_STRATEGY=cp dgoss run -e PACKAGE=\"#{repo}\" -e VERSION=\"#{version}\" semaphoreci/#{repo}:#{tag} /bin/sleep 3600")
+      @logger.info("Push to Dockerhub")
+      self.run("docker push semaphoreci/#{repo}:#{tag}")
     end
   end
+
 end
 
 options = OpenStruct.new
 parser = OptionParser.new do |opts|
   opts.banner = 'Usage: %s [options]' % $0
   opts.on('-d', '--dir TAG', 'Dockerfiles dir.') { |o| options[:dir] = o }
+  opts.on('-r', '--rebuild', 'Rebuild all images') { |o| options[:rebuild] = o }
   opts.on_tail("-h", "--help", "Show help") do
     puts opts
     exit 1
@@ -85,4 +84,4 @@ rescue OptionParser::ParseError => e
   exit 1
 end
 
-SemaphoreRegistry.build(options[:dir])
+SemaphoreRegistry.build(options[:dir],options[:rebuild])
