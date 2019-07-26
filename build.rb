5899a38da8656c90cf6b261aa6017a77799f112b
CommanderK5 <CommanderK5@users.noreply.github.com>
2019-07-26 09:39:19 +0200

    Cleanup (#37)
    * lint and style
    
    * update build pipeline

--- build.rb
+++ build.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'json'
 require 'net/http'
 require 'optparse'
@@ -6,18 +8,15 @@ require 'ostruct'
 require 'logger'
 require 'open3'
 
-
 class SemaphoreRegistry
-
   @logger = Logger.new(STDOUT)
   @logger.level = Logger::INFO
 
-  def self.search(name,tag)
+  def self.search(name, tag)
     uri = URI("https://registry.hub.docker.com/v2/repositories/semaphoreci/#{name}/tags/#{tag}/")
     puts uri
     response = Net::HTTP.get_response(uri)
     if response.is_a?(Net::HTTPSuccess)
-      body = JSON.parse(Net::HTTP.get(uri))
       @logger.info("Image found: Repository: #{name}, Tag: #{tag}")
       return true
     elsif response.is_a?(Net::HTTPNotFound)
@@ -30,58 +29,55 @@ class SemaphoreRegistry
   end
 
   def self.run(cmd)
-    Open3.popen2(cmd) do |stdin, stdout_stderr, wait_thr|
-      stdout_stderr.each {|l| @logger.info l }
+    Open3.popen2(cmd) do |_stdin, stdout_stderr, wait_thr|
+      stdout_stderr.each { |l| @logger.info l }
       exit_status = wait_thr.value
-      unless exit_status.success?
-        abort "FAILED !!! #{cmd}"
-      end
+      abort "FAILED !!! #{cmd}" unless exit_status.success?
     end
   end
 
-  def self.build(dir,rebuild,test)
+  def self.build(dir, rebuild, test)
     Dir["#{dir}/*"].each do |f|
       next if File.directory?(f)
+
       # e.g Dockerfile-golang-1.9
-      parts = File.basename(f).split("-")
+      parts = File.basename(f).split('-')
       repo = parts[1]
-      # Get tag from filename - 1.9-node
-      version = parts[2]
-      tag = parts[2...parts.length].map { |k| k }.join("-")
-      next if self.search(repo,tag) && !rebuild
-      @logger.info("Rebuilding all Images") if rebuild
+      tag = parts[2...parts.length].map { |k| k }.join('-')
+      next if search(repo, tag) && !rebuild
+
+      @logger.info('Rebuilding all Images') if rebuild
       @logger.info("Building #{repo} #{tag}")
-      self.run("docker build -t semaphoreci/#{repo}:#{tag} -f #{f} #{dir}")
-      @logger.info("Running Tests")
-      self.run("GOSS_FILES_PATH=tests/goss GOSS_VARS=vars.yaml GOSS_FILES_STRATEGY=cp dgoss run -e PACKAGE=\"#{repo}\" semaphoreci/#{repo}:#{tag} /bin/sleep 3600")
-      if !test
-        @logger.info("Push to Dockerhub")
-        self.run("docker push semaphoreci/#{repo}:#{tag}")
+      run("docker build -t semaphoreci/#{repo}:#{tag} -f #{f} #{dir}")
+      @logger.info('Running Tests')
+      run("GOSS_FILES_PATH=tests/goss GOSS_VARS=vars.yaml GOSS_FILES_STRATEGY=cp dgoss run -e PACKAGE=\"#{repo}\" semaphoreci/#{repo}:#{tag} /bin/sleep 3600")
+      unless test
+        @logger.info('Push to Dockerhub')
+        run("docker push semaphoreci/#{repo}:#{tag}")
       end
-      @logger.info("Cleanup")
-      self.run("docker system prune -a -f")
+      @logger.info('Cleanup')
+      run('docker system prune -a -f')
     end
   end
-
 end
 
 options = OpenStruct.new
 parser = OptionParser.new do |opts|
-  opts.banner = 'Usage: %s [options]' % $0
+  opts.banner = format('Usage: %s [options]', $PROGRAM_NAME)
   opts.on('-d', '--dir DIR', 'Dockerfiles dir.') { |o| options[:dir] = o }
   opts.on('-t', '--test', 'Only test images with dgoss') { |o| options[:test] = o }
   opts.on('-r', '--rebuild', 'Rebuild all images') { |o| options[:rebuild] = o }
-  opts.on_tail("-h", "--help", "Show help") do
+  opts.on_tail('-h', '--help', 'Show help') do
     puts opts
     exit 1
   end
 end
 
-args = parser.parse!
+parser.parse!
 
 begin
   mandatory = [:dir]
-  missing = mandatory.select{ |param| options[param].nil? }
+  missing = mandatory.select { |param| options[param].nil? }
   raise OptionParser::MissingArgument, missing.join(', ') unless missing.empty?
 rescue OptionParser::ParseError => e
   puts e
@@ -89,4 +85,4 @@ rescue OptionParser::ParseError => e
   exit 1
 end
 
-SemaphoreRegistry.build(options[:dir],options[:rebuild],options[:test])
+SemaphoreRegistry.build(options[:dir], options[:rebuild], options[:test])
